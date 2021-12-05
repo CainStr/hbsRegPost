eb3cda8c19ece2c003827ab06e10ca08b802162d
Andrew <isofthetruth@gmail.com>
2021-12-05 09:37:02 +0300

    перекинул на роут разметку

--- public/main.js
+++ public/main.js
@@ -8,17 +8,7 @@ const $postsWr = document.querySelector('[data-postsWr]')
 
 // создаем функцию с разметкой  в виде строки с шаблонными строками, в которую передадим аргументом объект и вытащим константы через деструктуризацию проверяем, что  в hbs  естьт data- атрибут для обращения к нему, копируем разметку 
 
-const generateHtmlForPost = (postOb) => {
-  // Вставляем разметку из hbs  и меняем {{{}}}на шаблонные строки `${} и this на аргумент передаваемый`
-  return `<div data-id = "${postOb.id}" class="card my-4" style="width: 18rem;">
-			<img src="${postOb.picture}" class="card-img-top" alt="${postOb.title}">
-			<div class="card-body">
-				<h5 class="card-title">${postOb.title}</h5>
-				<p class="card-text">${postOb.description}</p>
-				<a data-action="delete" href="#" class="btn btn-danger">Delete</a>
-			</div>
-		</div>`
-}
+
 
 // вешаем на форму оброботчик событий submit
 //сталкиваемся с проблемой , что main.js подключен ко всем страницам через layout , при переходе на другие страницы , где нет формы будет вылетать ошибка, для устранения этой проблемы нужно добавить "оператор опциональной последовательности (для  проверки наличия на странице искомого ДОМ элемента"
@@ -43,9 +33,9 @@ $postAddForm?.addEventListener('submit', async (event) => {
   //Дожидаемся второго эапа феча ( объекта)
   const newPostFromServer = await response.json()
 
-
+console.log(newPostFromServer)
   // метод вставляющий html разметку в DOM дерево (буквально переводится вставить по соседству ) ( аргументом принимается место куда я хочу вставить  )
-  $postsWr.insertAdjacentHTML('afterbegin', generateHtmlForPost(newPostFromServer))
+  $postsWr.insertAdjacentHTML('afterbegin', newPostFromServer)
 })
 // вешаем обрабочик собыий на $postsWr --> внешний блок  
 $postsWr?.addEventListener('click', async (event) => {
--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -11,7 +11,19 @@ postsRouter.route('')
       console.log(req.body)
       const { title, picture, description } = req.body
       const newPost = await Post.create({ title, picture, description })
-      res.json(newPost)
+      const generateHtmlForPost = (newPost) => {
+        // Вставляем разметку из hbs  и меняем {{{}}}на шаблонные строки `${} и this на аргумент передаваемый`
+        return `<div data-id = "${newPost.id}" class="card my-4" style="width: 18rem;">
+            <img src="${newPost.picture}" class="card-img-top" alt="${newPost.title}">
+            <div class="card-body">
+              <h5 class="card-title">${newPost.title}</h5>
+              <p class="card-text">${newPost.description}</p>
+              <a data-action="delete" href="#" class="btn btn-danger">Delete</a>
+            </div>
+          </div>`
+      }
+      
+      res.json(generateHtmlForPost(newPost))
     } catch (error) {
       res.sendStatus(400)
     }
